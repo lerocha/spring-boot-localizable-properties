a644d384929aa7f7b13136650a25784d18cdcafe
Luis Rocha <lerocha@gmail.com>
2016-10-10 21:43:53 -0700

    adding endpoint to get by locale

--- src/main/java/com/github/lerocha/controller/PropertiesController.java
+++ src/main/java/com/github/lerocha/controller/PropertiesController.java
@@ -2,9 +2,12 @@ package com.github.lerocha.controller;
 
 import com.github.lerocha.properties.RegionProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * Created by lerocha on 10/8/16.
@@ -18,5 +21,10 @@ public class PropertiesController {
     @RequestMapping(value = "properties", method = RequestMethod.GET)
     public RegionProperties getProperties() {
         return regionProperties;
+    }
+
+    @RequestMapping(value = "properties/{locale}", method = RequestMethod.GET)
+    public RegionProperties getProperties(@PathVariable Locale locale) {
+        return regionProperties.fromLocale(locale);
     }
 }
